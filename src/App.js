35a6452d931769908c1f805a3d91f3de5ad7513e
=A-sushma15 <=>
2024-07-05 12:19:07 +0530

    Initialized

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Home from './components/home/Home';
+import NewsComponent from'./components/news/NewsComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> hello world </h1>
+      
+      <Home/>
+      
+    </div>
+   
+  );
+}
+
+export default App;
